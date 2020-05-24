c41e6bae65298ed6fb5bbfd5dbe25aa35eb2d208
Harinath <hari@Haris-MacBook-Pro.local>
2020-05-25 02:26:43 +0530

    first commit

---
+++ api_project.js
@@ -0,0 +1,17 @@
+define({
+  "name": "ToDo Application",
+  "version": "1.0.0",
+  "description": "Documentaion for ToDo Application",
+  "Version": "1.0.0",
+  "title": "REST API's",
+  "url": "http://api.todoapps.online",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-05-24T20:36:07.881Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
